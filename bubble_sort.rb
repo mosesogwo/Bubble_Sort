44f7a93733ba599517d13c985b1094291189a553
Moses Ogwo <mosesogwo@gmail.com>
2019-06-10 12:08:23 +0100

    bubble_sort_by method added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -22,3 +22,28 @@ def bubble_sort(arr)
 end
 
 p bubble_sort([6, 5, 3, 9, 0])
+
+def bubble_sort_by(arr)
+  passes = arr.length
+  pass = 0
+  last_index = passes - (2 + pass)
+  while pass < passes
+    swaps = 0
+    arr.each_with_index { |ele, index|
+      if index <= last_index
+        diff = yield arr[index], arr[index+1]
+        swap(arr, index, index + 1) and swaps += 1 if diff > 0
+      end
+    }
+    break if swaps.equal?(0)
+    p arr
+    pass += 1
+    last_index = passes - (2 + pass)
+  end
+  arr
+end
+
+bubble_sort_by(["hia","hello", "hib", "arsrejdjshshsll", "hey","bubble"]) do |left,right|
+  left.length <=> right.length
+end
+
