9b08b0ff71ed64edae3ebbabcedebb857002f02b
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-10 12:27:03 +0200

    Optimize using swap count on each iteration and limit of last index

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -7,12 +7,16 @@ end
 def bubble_sort(arr)
   passes = arr.length
   pass = 0
-  last_index = arr.length - 2
+  last_index = passes - (2 + pass)
   while pass < passes
+    swaps = 0
     arr.each_with_index { |ele, index|
-      swap(arr, index, index + 1) if index <= last_index && arr[index] > arr[index + 1]
+      swap(arr, index, index + 1) and swaps += 1 if index <= last_index && arr[index] > arr[index + 1]
     }
+    break if swaps.equal?(0)
+    p arr
     pass += 1
+    last_index = passes - (2 + pass)
   end
   arr
 end
