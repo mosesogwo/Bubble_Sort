8adb8c57d09b6332c768731940d2ed479f119e81
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-10 12:17:03 +0200

    Initial bubble sort - non optimized

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -8,10 +8,13 @@ def bubble_sort(arr)
   passes = arr.length
   pass = 0
   last_index = arr.length - 2
-
-  arr.each_with_index { |ele, index|
-    swap(arr, index, index + 1) if index <= last_index && arr[index] > arr[index + 1]
-  }
+  while pass < passes
+    arr.each_with_index { |ele, index|
+      swap(arr, index, index + 1) if index <= last_index && arr[index] > arr[index + 1]
+    }
+    pass += 1
+  end
+  arr
 end
 
 p bubble_sort([6, 5, 3, 9, 0])
