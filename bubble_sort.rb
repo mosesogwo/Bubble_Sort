038dd7f1e5cf748e5f9b2804cbc7f9d7926a3465
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-10 12:08:04 +0200

    Remove DbContextFactory and add injection via SimpleInjector

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,10 +1,26 @@
-def bubble_sort(arr)
-  n = arr.length
-  # i = 0
-  # while (i < n)
-    for element in arr
-      p element
-    end
+def swap(arr, source, destination)
+  temp = arr[source]
+  arr[source] = array[destination]
+  arr[destination] = temp
 end
 
- p bubble_sort([1, 2, 3])
+def bubble_sort(arr)
+  passes = arr.length
+  pass = 0
+  last_index = arr.length - 2
+  # n = arr.length
+  # i = 0
+  #while (pass < passes)
+  arr.each_with_index { |ele, index|
+    p index
+    swap(arr, index, index + 1) if index <= last_index && arr[index] > arr[index + 1]
+  }
+  p arr
+  #end
+
+  # for element in arr
+  #   p element
+  # end
+end
+
+p bubble_sort([6, 5, 4]) # = 3, 2,
