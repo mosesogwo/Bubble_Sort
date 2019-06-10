6f6ffd11e6ee79cb04223af519c3ad549e4f14a1
Edward Iga Kigongo <igakigongo@gmail.com>
2019-06-10 15:45:49 +0200

    Refactor some code in the bubble-sort that uses blocks

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # check if we can swap - emits swap
-def can_swap?(arr, index, last_index)
-  index <= last_index && arr[index] > arr[index + 1]
+def can_swap?(arr, i, max_i)
+  i <= max_i && arr[i] > arr[i + 1]
 end
 
 # swap 2 elements between source and destination indices of an array
@@ -11,15 +11,16 @@ def swap(arr, source, destination, swaps)
   temp = arr[source]
   arr[source] = arr[destination]
   arr[destination] = temp
+  swaps
 end
 
 # merge swap with can_swap utility
-def swap_if_possible(arr, index, max_index, swaps)
-  if can_swap?(arr, index, max_index)
+def swap_if_possible(arr, i, max_index, swaps)
+  if can_swap?(arr, i, max_index)
     swaps += 1
-    temp = arr[index]
-    arr[index] = arr[index + 1]
-    arr[index + 1] = temp
+    temp = arr[i]
+    arr[i] = arr[i + 1]
+    arr[i + 1] = temp
   end
   swaps
 end
@@ -40,20 +41,15 @@ end
 p bubble_sort([6, 5, 3, 9, 0])
 
 def bubble_sort_by(arr)
-  passes = arr.length
   pass = 0
-  last_index = passes - (2 + pass)
-  while pass < passes
+  loop do
     swaps = 0
-    arr.each_with_index do |ele, index|
-      if index <= last_index
-        diff = yield ele, arr[index + 1]
-        swap(arr, index, index + 1, swaps) and swaps += 1 if diff.positive?
-      end
+    max_i = arr.length - (2 + pass)
+    arr.each_with_index do |ele, i|
+      swaps = swap(arr, i, i + 1, swaps) if i <= max_i && (yield ele, arr[i + 1]).positive?
     end
-    break if swaps.equal?(0)
     pass += 1
-    last_index = passes - (2 + pass)
+    break if swaps.equal?(0) || pass.equal?(arr.length)
   end
   arr
 end
